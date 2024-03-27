3593bf7819a2cc8a48c34ed8420d00aebcd76d3e
Sobhan Zadehali <99137872+soza780@users.noreply.github.com>
2024-03-27 12:46:31 +0000

    initial order app commit

--- myshop/cart/urls.py
+++ myshop/cart/urls.py
@@ -5,6 +5,6 @@ app_name = 'cart'
 
 urlpatterns = [
     path('', views.cart_detail, name='cart_detail'),
-    path('add/<int:prosuct_id>/', views.cart_add, name='cart_add'),
-    path('remove/<int:prosuct_id>/', views.cart_remove, name='cart_remove'),
+    path('add/<int:product_id>/', views.cart_add, name='cart_add'),
+    path('remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
 ]
