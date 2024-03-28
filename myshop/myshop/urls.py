5cb81ad39b935f32cb1526c6092b4805f773842e
Sobhan Zadehali <99137872+soza780@users.noreply.github.com>
2024-03-28 07:15:21 +0000

    chekout templates added

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -21,10 +21,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
-
-
-
 ]
 
 if settings.DEBUG:
--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -22,6 +22,6 @@ def order_create(request):
             return render(request,
                           'orders/order/created.html',
                           {'order': order})
-        else:
-            form = OrderCreateForm()
-        return render(request, 'orders/order/created.html', {'cart': cart, 'form': form})
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
