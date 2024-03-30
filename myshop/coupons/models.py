54514fece4c1aed4bef4171fabb373636672bbe5
Sobhan Zadehali <99137872+soza780@users.noreply.github.com>
2024-03-30 15:47:12 +0000

    coupon model

--- myshop/coupons/models.py
+++ myshop/coupons/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    valid_from = models.DateField()
+    valid_to = models.DateField()
+    discount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)],
+                                   help_text="percentage val")
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.code
