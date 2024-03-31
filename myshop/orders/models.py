fad87900a2caaa76fb97962754e7f586f09594e9
Sobhan Zadehali <99137872+soza780@users.noreply.github.com>
2024-03-31 19:30:16 +0000

    coupon added to order

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -1,10 +1,17 @@
+from decimal import Decimal
+
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from shop.models import Product
+
+from coupons.models import Coupon
 
 
 # Create your models here.
 
 class Order(models.Model):
+    coupon = models.ForeignKey(Coupon, on_delete=models.CASCADE, related_name='orders', null=True, blank=True)
+    discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
@@ -26,19 +33,28 @@ class Order(models.Model):
         return f'Order {self.id}'
 
     def get_total_cost(self):
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()
+
+    def get_total_cost_before_discount(self):
         return sum(item.get_cost() for item in self.items.all())
+
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+
+        if self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        return Decimal(0)
 
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
-    product = models.ForeignKey(Product,on_delete=models.CASCADE, related_name='order_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
     price = models.DecimalField(max_digits=15, decimal_places=0)
     quantity = models.PositiveIntegerField()
 
     def __str__(self):
         return str(self.id)
-    
-    def get_cost(self):
-        return self.price * self .quantity
 
-    
+    def get_cost(self):
+        return self.price * self.quantity
